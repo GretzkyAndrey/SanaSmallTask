4b755888bcaeb61274f00acf145ad4fb33753771
Andrii Hretskyi <andreygretzky@gmail.com>
2022-04-02 02:49:48 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SanaFirstApp.Models;
+using System.Diagnostics;
+
+namespace SanaFirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index2(string name)
+        {
+            
+            if (name != null)
+            {
+                ViewData["Message"] = "Hello " + name;
+                return View("Result");
+            }
+
+            return View("Test");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
